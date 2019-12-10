613f9ebe26ccb582a370708e1c67de20a52b5dbd
1800617 <andra.pillesson@gmail.com>
2019-12-10 21:33:48 +0200

    reordered stuff and added comments

--- SQL_create_alarms.py
+++ SQL_create_alarms.py
@@ -5,11 +5,9 @@ c = conn.cursor()
 
 c.execute('''CREATE TABLE alarms (id integer PRIMARY KEY AUTOINCREMENT,time text NOT NULL)''')
 
-alarms = [('02:15'), ('02:30'), ('07:15'), ('12:34')]
 c.execute("INSERT INTO alarms (time) VALUES ('07:15')")
 c.execute("INSERT INTO alarms (time) VALUES ('09:30')")
-c.execute("INSERT INTO alarms (time) VALUES ('10:50')")
-c.execute("INSERT INTO alarms (time) VALUES ('12:34')")
+c.execute("INSERT INTO alarms (time) VALUES ('11:30')")
 
 conn.commit()
 
--- alarm_clock.py
+++ alarm_clock.py
@@ -10,17 +10,10 @@ print("This is the alarm clock.")
 print("Type 'HELP' for commands")
 
 
-def get_alarms():
-    alarms = []
-    for row in c.execute('SELECT time FROM alarms'):
-        alarm = str(row).strip("()',")
-        alarms.append(alarm)
-    print("get_alarms ", alarms)
-    return alarms
-
-
 def main():
+    # start timers outside the loop
     timer()
+
     while True:
         user_input = input("Enter a command: ")
 
@@ -41,84 +34,82 @@ def main():
         elif user_input in ["list", "LIST"]:
             show_list()
 
-        elif user_input in ["delete", "DELETE", "del"]:
-            show_list()
-            delete()
-            main()
-            print("Alarm deleted ")
-#BALEET
-        elif user_input in ["qwe"]:
-            get_alarms()
-
         elif user_input in ["set", "SET"]:
-            set()
+            set_alarm()
             timer()
             show_list()
 
+        elif user_input in ["delete", "DELETE", "del"]:
+            show_list()
+            delete_alarm()
+            print("Alarm deleted ")
+            main()
 
-def show_list():
-    for row in c.execute('SELECT * FROM alarms'):
-        print(row[0], " : ", row[1])
+
+def timer():
+    # get list of alarms
+    alarm_list = get_alarms()
+
+    # loop through rows in alarm_list
+    for alarm_string in alarm_list:
+
+        # get seconds left until alarm for each row
+        seconds_left = get_seconds(alarm_string)
+
+        # create and start timer
+        if seconds_left >= 0:
+            # after x seconds call 'play_alarm'
+            alarm_timer = threading.Timer(seconds_left, play_alarm)
+            alarm_timer.start()
 
 
-def play_alarm():
-    print("Wake up!")
-    fname = "C:\\Windows\\Media\\Alarm04.wav"
-    winsound.PlaySound(fname, winsound.SND_FILENAME)
+def get_alarms():
+    # create empty alarms list
+    alarms = []
+
+    # get alarm times from database and add them to alarms list
+    for row in c.execute('SELECT time FROM alarms'):
+        alarm = str(row).strip("()',")  # remove unnecessary characters
+        alarms.append(alarm)
+
+    return alarms
 
 
 def get_seconds(alarm_string):
+    # split alarm_string into array
     time = alarm_string.split(":")
+
+    # alarm seconds
     a_hours = int(time[0])
     a_minutes = int(time[1])
     a_seconds = (a_hours * 3600 + a_minutes * 60)
 
+    # current seconds
     c_hours = int(strftime("%H"))
     c_minutes = int(strftime("%M"))
     c_seconds = (c_hours * 3600 + c_minutes * 60 + int(strftime("%S")))
 
+    # calculate seconds left until alarm
     seconds_left = a_seconds - c_seconds
-    print(alarm_string, " ", seconds_left)
     return seconds_left
 
 
-def timer():
-    alarm_list = get_alarms()
-    for alarm_string in alarm_list:
-        seconds_left = get_seconds(alarm_string)
-        if seconds_left >= 0:
-            alarm_timer = threading.Timer(get_seconds(alarm_string), play_alarm)
-            alarm_timer.start()
+def play_alarm():
+    print("Wake up!")
+    # alarm sound location
+    fname = "C:\\Windows\\Media\\Alarm04.wav"
+    # play alarm sound
+    winsound.PlaySound(fname, winsound.SND_FILENAME)
 
 
-def insert(new_alarm):
-    c.execute('INSERT INTO alarms (time) VALUES (?)', (new_alarm,))
+def show_list():
+    # get list of alarms from database and print them out
+    for row in c.execute('SELECT * FROM alarms'):
+        print(row[0], " : ", row[1])
 
 
-def delete():
-    id_list = []
-    for ids in c.execute('SELECT id FROM alarms'):
-        id_string = str(ids).strip('(,)')
-        id_string = int(id_string)
-        id_list.append(id_string)
-    print(id_list)
-
-    answer = 0
-    while not answer:
-        try:
-            answer = int(input("Pick a number to delete: "))
-            if answer not in id_list:
-                raise ValueError
-        except ValueError:
-            answer = 0
-            print("That is not an option")
-    print(answer)
-    answer = str(answer)
-
-    c.execute('DELETE FROM alarms WHERE id = (?)', (answer,))
-
-
-def set():
+def set_alarm():
+    # get input from user and check that it's valid
     set_hours = 0
     hour_set = False
     while not hour_set:
@@ -132,6 +123,7 @@ def set():
             set_hours = 0
             print("That is not an option")
 
+    # get input from user and check that it's valid
     set_minutes = 0
     minute_set = False
     while not minute_set:
@@ -145,17 +137,41 @@ def set():
             set_minutes = 0
             print("That is not an option")
 
+    # create new_alarm from user inputs
     set_hours = str(set_hours).zfill(2)
     set_minutes = str(set_minutes).zfill(2)
     new_alarm = """""" + set_hours + ":" + set_minutes + """"""
 
-    print(new_alarm)
-
+    # insert new_alarm into database
     c.execute('INSERT INTO alarms (time) VALUES (?)', (new_alarm,))
     conn.commit()
 
     print("New alarm set at: ", new_alarm)
 
+
+def delete_alarm():
+    # get ids from database, reformat them and add them to id_list
+    id_list = []
+    for ids in c.execute('SELECT id FROM alarms'):
+        id_string = str(ids).strip('(,)')
+        id_string = int(id_string)
+        id_list.append(id_string)
+    print(id_list)
+
+    # get id as user input and check that it's valid
+    answer = 0
+    while not answer:
+        try:
+            answer = int(input("Pick a number to delete: "))
+            if answer not in id_list:
+                raise ValueError
+        except ValueError:
+            answer = 0
+            print("That is not an option")
+    answer = str(answer)
+
+    # remove from database
+    c.execute('DELETE FROM alarms WHERE id = (?)', (answer,))
 
 
 main()
